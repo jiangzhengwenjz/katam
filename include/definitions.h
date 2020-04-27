e07d3491c06fd5c2fde98cdef5ad2989eeed64e9
Egor Ananyin <ananinegor@gmail.com>
2020-04-27 16:58:31 +0300

    Decompile more of main.c

--- include/definitions.h
+++ include/definitions.h
@@ -1,3 +1,11 @@
+u8 gUnk_0300248C;
+u32 gUnk_03002440;
+u32* gUnk_03002484;
+u32 gUnk_03002760[0xa0];
+u32* gUnk_03002EAC;
+u8 gUnk_03006070;
+u32 gUnk_030060B0[0x100];
+
 void sub_0815158C();
 void sub_080001CC();
 void sub_08151C54();
--- src/main.c
+++ src/main.c
@@ -1,5 +1,74 @@
 #include "global.h"
 
+void VCountIntr(){
+    REG_IF = INTR_FLAG_VCOUNT;
+}
+
+void Dma0Intr(){
+    REG_IF = INTR_FLAG_DMA0;
+}
+
+void Dma1Intr(){
+    REG_IF = INTR_FLAG_DMA1;
+}
+
+void Dma2Intr(){
+    REG_IF = INTR_FLAG_DMA2;
+}
+
+void Dma3Intr(){
+    REG_IF = INTR_FLAG_DMA3;
+}
+
+void Timer0Intr(){
+    REG_IF = INTR_FLAG_TIMER0;
+}
+
+void Timer1Intr(){
+    REG_IF = INTR_FLAG_TIMER1;
+}
+
+void Timer2Intr(){
+    REG_IF = INTR_FLAG_TIMER2;
+}
+
+void Timer3IntrDummy(){
+    REG_IF = INTR_FLAG_TIMER3;
+}
+
+void KeypadIntr(){
+    REG_IF = INTR_FLAG_KEYPAD;
+}
+
+void GamepakIntr(){
+    REG_IF = INTR_FLAG_GAMEPAK;
+}
+
+void nullsub_142(){
+    
+}
+
+void sub_08152968(){
+    u32 filler;
+    gUnk_0300248C = 0;
+    gUnk_03002440 &= ~8;
+    if(!(gUnk_03002440 & 0x20)){
+        if(gUnk_03002484 == &gUnk_03002760[0]){
+            gUnk_03002484 = &gUnk_03002760[0xa0];
+            gUnk_03002EAC = &gUnk_03002760[0];
+        }
+        else{
+            gUnk_03002484 = &gUnk_03002760[0];
+            gUnk_03002EAC = &gUnk_03002760[0xa0];
+        }
+    }
+    gUnk_03002440 &= ~4;
+    filler = 0x200;
+    CpuFastSet(&filler, gUnk_030060B0, 0x01000100);
+    gUnk_03006070 = 0;
+    gUnk_03002440 &= ~0x10;
+}
+
 void AgbMain(){
     sub_0815158C();
     sub_080001CC();
