790fe14933bc828e077e26e3f976c9f5d16b81a1
Egor Ananyin <ananinegor@gmail.com>
2020-05-01 12:02:05 +0300

    Main.c done!

--- include/functions.h
+++ include/functions.h
@@ -8,5 +8,10 @@ extern void sub_08030E44(void);
 extern u32 sub_08030FE0(void);
 extern void sub_08032E98(void);
 extern void nullsub_2(void);
+extern void sub_08158870(void);
+extern void sub_08159074(void);
+extern void sub_08152A18(void);
+extern void sub_08157168(void);
+extern void IntrMain(void);
 
 #endif
