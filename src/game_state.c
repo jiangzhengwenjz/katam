fb3f0d2f7f27f465743ce4347084724efd9c71df
Cleverking2003 <ananinegor@gmail.com>
2020-05-03 22:17:58 +0300

    Decompile sub_08152C3C

--- src/game_state.c
+++ src/game_state.c
@@ -2,6 +2,47 @@
 #include "game_state.h"
 #include "gba/m4a_internal.h"
 
+void sub_08152C3C(struct GameState* arg0) {
+    u32 r0, r1;
+    u16 r2;
+    if (!(arg0->unk12 & 2)) {
+        r1 = arg0->unk2 + IWRAM_START;
+        r0 = arg0->unk4 + IWRAM_START;
+        if (r1 != IWRAM_START) {
+            if (r0 != IWRAM_START) {
+                if (arg0->unkC != NULL) {
+                    arg0->unkC(arg0);
+                }
+                if (arg0 == gUnk_03002EBC) {
+                    gUnk_03002EBC = (struct GameState*)(arg0->unk4 + IWRAM_START);
+                }
+                if (arg0 == gUnk_03002E98) {
+                    gUnk_03002E98 = (struct GameState*)(arg0->unk4 + IWRAM_START);
+                }
+                r1 = arg0->unk2 + IWRAM_START;
+                r0 = arg0->unk4 + IWRAM_START;
+                ((struct GameState*)(r1))->unk4 = r0;
+                ((struct GameState*)(r0))->unk2 = r1;
+                if (arg0->unk6 != 0) {
+                    if (arg0->unk12 & 0x10) {
+                        sub_081590EC(arg0->unk6 + (u32*)EWRAM_START);
+                    }
+                    else {
+                        sub_08152E40(arg0->unk6 + (u8*)IWRAM_START);
+                    }
+                }
+                gUnk_03002560[--gUnk_03002E7C] = arg0;
+                arg0->unk0 = 0;
+                arg0->unk2 = 0;
+                arg0->unk8 = nullsub_145;
+                arg0->unk10 = 0;
+                arg0->unk12 = 0;
+                arg0->unk6 = 0;
+            }
+        }
+    }
+}
+
 void sub_08152CF4(void) {
     gUnk_030035D0 = gUnk_03002560[0];
     if (!(gUnk_03002440 & 0x800) && (gUnk_03002560[0] != (struct GameState*)IWRAM_START)) {
