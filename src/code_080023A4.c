355f62180e262f439e46f9d9dd657a0f83105d09
jiangzhengwenjz <jiangzhengwenjzw@qq.com>
2020-05-26 11:51:07 -0500

    indent

--- src/code_080023A4.c
+++ src/code_080023A4.c
@@ -5,8 +5,8 @@
 
 void sub_080023A4(u8 arg0) {
     struct Unk_02023530* r1 = gUnk_02023530 + arg0;
-	if (r1->current_room != 0xffff &&
-		(gUnk_0203AD10 & 1) == 0) {
-		sub_0800A08C(arg0);
-	}
+    if (r1->current_room != 0xffff &&
+        (gUnk_0203AD10 & 1) == 0) {
+        sub_0800A08C(arg0);
+    }
 }
