4825e421d50852b37d2ce8236c3d9935e671f6a7
Egor Ananyin <ananinegor@gmail.com>
2020-06-01 19:49:15 +0300

    Make code nicer

--- src/code_0800A868.c
+++ src/code_0800A868.c
@@ -6,30 +6,27 @@
 s32 sub_0800A868(u8 playerId, struct Object *obj)
 {
     s32 *var0;
-    s32 (**var1)(struct Object *, u8);
-    s32 var2;
+    s32 var1;
 
     var0 = sub_08002958(playerId);
 
     switch (obj->unk00) {
     case 1:
-        var1 = gUnk_0835107C;
-        var2 = var1[obj->type](obj, playerId);
-        var0[2] = var2 + 0x56;
+        var1 = gUnk_0835107C[obj->type](obj, playerId);
+        var0[2] = var1 + 0x56;
         break;
     case 2:
-        var1 = gUnk_082D91D4;
-        var2 = var1[obj->type](obj, playerId);
-        var0[2] = var2 + 0x56;
+        var1 = gUnk_082D91D4[obj->type](obj, playerId);
+        var0[2] = var1 + 0x56;
         break;
     default:
-        var2 = 0;
+        var1 = 0;
         break;
     }
 
     sub_08002918(playerId, var0);
 	
-    return var2;
+    return var1;
 }
 
 void sub_0800A8BC(void)
