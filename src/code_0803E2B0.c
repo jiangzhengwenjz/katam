6181bc89355a71062456e0b495970413d4f6743e
Solitai7e <p5ychomanti5@protonmail.com>
2020-08-30 04:39:12 +0000

    Decompile sub_0803E2B0 and sub_0803E308

---
+++ src/code_0803E2B0.c
@@ -0,0 +1,21 @@
+#include "global.h"
+#include "data.h"
+#include "functions.h"
+
+// TODO: define file boundaries
+
+void sub_0803E2B0(struct Object2 *arg0, s8 arg1, s8 arg2, s8 arg3, s8 arg4)
+{
+    arg0->unk38 = arg1;
+    arg0->unk39 = arg2;
+    arg0->unk3A = ((arg3 - arg1 < 0)? arg1 - arg3 : arg3 - arg1) >> 1;
+    arg0->unk3B = ((arg4 - arg2 < 0)? arg2 - arg4 : arg4 - arg2) >> 1;
+}
+
+void sub_0803E308(struct Object2 *arg0, s8 arg1, s8 arg2, s8 arg3, s8 arg4)
+{
+    arg0->unk3C = arg1;
+    arg0->unk3D = arg2;
+    arg0->unk3E = arg3;
+    arg0->unk3F = arg4;
+}
