2936b1d0d0e980470ee02188e6f04f2b75eced1b
Egor Ananyin <ananinegor@gmail.com>
2020-11-29 20:02:08 +0300

    changes

--- include/laser_ball.h
+++ include/laser_ball.h
@@ -1,46 +1,12 @@
-#ifndef GUARD_LASER_BALL
-#define GUARD_LASER_BALL
+#ifndef GUARD_LASER_BALL_H
+#define GUARD_LASER_BALL_H
 
 #include "data.h"
 #include "global.h"
 #include "object.h"
 
 struct Laser {
-    u8 unk0;
-    u8 unk1;
-    u16 filler2;
-    s16 counter;
-    u8 filler6[2];
-    u32 flags;
-    u32 unkC;
-    struct Sprite unk10;
-    s8 unk38;
-    s8 unk39;
-    s8 unk3A;
-    s8 unk3B;
-    s8 unk3C;
-    s8 unk3D;
-    s8 unk3E;
-    s8 unk3F;
-    s32 x;
-    s32 y;
-    u32 unk48;
-    u32 unk4C;
-    s16 xspeed;
-    s16 yspeed;
-    u8 unk54;
-    u8 unk55;
-    u8 unk56;
-    u8 unk57;
-    u32 unk58;
-    u32 unk5C;
-    u16 unk60;
-    u8 unk62;
-    u8 unk63;
-    u16 unk64;
-    u16 unk66;
-    u32 unk68;
-    struct Kirby* kirby1;
+    struct ObjectBase base;
     struct Object2* parent;
     u8 filler74[4];
     u32 (*unk78)(struct Laser* arg0);
--- src/laser_ball.c
+++ src/laser_ball.c
@@ -251,18 +251,18 @@ void sub_080B2710(struct Object2* arg0) {
 }
 
 void sub_080B2780(struct Object2* arg0) {
-    struct Task *task = TaskCreate(sub_08070580, sizeof(struct Laser), 0xd4 << 6, 0x10, sub_0803DCCC);
+    struct Task *task = TaskCreate(sub_08070580, sizeof(struct Laser), 0x3500, 0x10, sub_0803DCCC);
     struct Laser *laser2, *laser = TASK_GET_STRUCT_PTR(task, laser2);
     sub_0803E380((struct Object2*)laser);
-    laser->unk0 = 2;
-    laser->x = arg0->x;
-    laser->y = arg0->y;
+    laser->base.unk0 = 2;
+    laser->base.x = arg0->x;
+    laser->base.y = arg0->y;
     laser->parent = arg0;
-    laser->counter = 0;
-    laser->unk60 = arg0->unk60;
-    laser->unk56 = arg0->unk56;
-    if (gUnk_03000510.unk4 & ((1 << laser->unk56) | 0x10)) {
-        laser->flags |= 0x2000;
+    laser->base.counter = 0;
+    laser->base.unk60 = arg0->unk60;
+    laser->base.unk56 = arg0->unk56;
+    if (gUnk_03000510.unk4 & ((1 << laser->base.unk56) | 0x10)) {
+        laser->base.flags |= 0x2000;
     }
     laser->unk78 = sub_080B2A30;
     laser->unk7C = sub_080B2D74;
@@ -271,111 +271,111 @@ void sub_080B2780(struct Object2* arg0) {
     laser->unk88 = 0;
     laser->unk89 = 0;
     laser->unk8A = 0;
-    laser->unk63 = 1;
-    laser->flags |= 0x80 << 0x15;
-    laser->unk68 = 0x20000043;
-    laser->unk5C |= 0x80 << 0xc;
-    if (gUnk_03000510.unk4 & ((1 << laser->unk56) | 0x10)) {
-        laser->flags |= 0x2000;
+    laser->base.unk63 = 1;
+    laser->base.flags |= 0x10000000;
+    laser->base.unk68 = 0x20000043;
+    laser->base.unk5C |= 0x80000;
+    if (gUnk_03000510.unk4 & ((1 << laser->base.unk56) | 0x10)) {
+        laser->base.flags |= 0x2000;
     }
     if (arg0->flags & 1) {
-        laser->xspeed = -0x800;
-        laser->flags |= 1;
-        laser->x -= 0x10;
+        laser->base.xspeed = -0x800;
+        laser->base.flags |= 1;
+        laser->base.x -= 0x10;
     }
     else {
-        laser->xspeed = 0x800;
-        laser->x += 0x10;
+        laser->base.xspeed = 0x800;
+        laser->base.x += 0x10;
     }
     sub_0803E2B0((struct Object2*)laser, 2, -3, 10, 3);
     sub_0803E308((struct Object2*)laser, 16, -1, 18, 1);
-    sub_080708DC((struct Object2*)laser, &laser->unk10, 4, 0x321, 6, 9);
-    laser->unk10.unk1F = 0;
+    sub_080708DC((struct Object2*)laser, &laser->base.unk10, 4, 0x321, 6, 9);
+    laser->base.unk10.unk1F = 0;
     if (arg0->unkC & 0x10) {
-        if (gUnk_02020EE0[gUnk_0203AD3C].base.unk60 == laser->unk60) {
-            laser->unk10.unk1F = sub_0803DF24(gUnk_08351648[50].unk8);
-            if (laser->unk10.unk1F == 0xff) {
-                laser->unk10.unk1F = sub_0803DFAC(gUnk_08351648[50].unk8, 0);
+        if (gUnk_02020EE0[gUnk_0203AD3C].base.unk60 == laser->base.unk60) {
+            laser->base.unk10.unk1F = sub_0803DF24(gUnk_08351648[50].unk8);
+            if (laser->base.unk10.unk1F == 0xff) {
+                laser->base.unk10.unk1F = sub_0803DFAC(gUnk_08351648[50].unk8, 0);
             }
         }
         else {
-            laser->unk10.unk1F = 0;
+            laser->base.unk10.unk1F = 0;
         }
     }
     else {
-        if (gUnk_02020EE0[gUnk_0203AD3C].base.unk60 == laser->unk60) {
-            laser->unk10.unk1F = sub_0803DF24(0x321);
-            if (laser->unk10.unk1F == 0xff) {
-                laser->unk10.unk1F = sub_0803DFAC(0x321, 0);
+        if (gUnk_02020EE0[gUnk_0203AD3C].base.unk60 == laser->base.unk60) {
+            laser->base.unk10.unk1F = sub_0803DF24(0x321);
+            if (laser->base.unk10.unk1F == 0xff) {
+                laser->base.unk10.unk1F = sub_0803DFAC(0x321, 0);
             }
         }
         else {
-            laser->unk10.unk1F = arg0->unkC & 0x10;
+            laser->base.unk10.unk1F = arg0->unkC & 0x10;
         }
     }
-    PlaySfx(laser, 0x9e * 2);
+    PlaySfx(&laser->base, 0x13c);
 }
 
 u32 sub_080B2A30(struct Laser* arg0) {
     struct Sprite sprite, *sprite2, *sprite3;
     struct Object2 *parent = arg0->parent;
-    if (gUnk_02020EE0[gUnk_0203AD3C].base.unk60 == arg0->unk60) {
-        if (arg0->flags & 0x4000) {
-            if (arg0->unk10.unk0 == 0) {
-                (sprite2 = &arg0->unk10)->unk0 = sub_0803DE54(4, arg0->unk10.unkC, arg0->unk10.unk1A);
-                sprite2->unk8 = arg0->unk10.unk8 & ~0x80000;
-                CpuCopy32(sprite2, &sprite, 0x28);
-                sub_0815521C(&sprite, arg0->unk1);
-                sprite2->unk8 = arg0->unk10.unk8 | 0x80000;
+    if (gUnk_02020EE0[gUnk_0203AD3C].base.unk60 == arg0->base.unk60) {
+        if (arg0->base.flags & 0x4000) {
+            if (arg0->base.unk10.unk0 == 0) {
+                (sprite2 = &arg0->base.unk10)->unk0 = sub_0803DE54(4, arg0->base.unk10.unkC, arg0->base.unk10.unk1A);
+                sprite2->unk8 = arg0->base.unk10.unk8 & ~0x80000;
+                CpuCopy32(sprite2, &sprite, sizeof(struct Sprite));
+                sub_0815521C(&sprite, arg0->base.unk1);
+                sprite2->unk8 = arg0->base.unk10.unk8 | 0x80000;
             }
         }
         else {
-            if (arg0->unk10.unk0 == 0) {
-                (sprite3 = &arg0->unk10)->unk0 = sub_081570B0(4);
-                sprite3->unk8 = arg0->unk10.unk8 & ~0x80000;
-                CpuCopy32(&arg0->unk10, &sprite, 0x28);
-                sub_0815521C(&sprite, arg0->unk1);
+            if (arg0->base.unk10.unk0 == 0) {
+                (sprite3 = &arg0->base.unk10)->unk0 = sub_081570B0(4);
+                sprite3->unk8 = arg0->base.unk10.unk8 & ~0x80000;
+                CpuCopy32(&arg0->base.unk10, &sprite, sizeof(struct Sprite));
+                sub_0815521C(&sprite, arg0->base.unk1);
             }
         }
     }
     else {
-        if (arg0->unk10.unk0 != 0) {
-            bool8 val = arg0->flags & 0x4000;
-            if (!(arg0->flags & 0x4000)) {
-                sub_08157190(arg0->unk10.unk0);
-                arg0->unk10.unk0 = val;
+        if (arg0->base.unk10.unk0 != 0) {
+            bool8 val = arg0->base.flags & 0x4000;
+            if (!(arg0->base.flags & 0x4000)) {
+                sub_08157190(arg0->base.unk10.unk0);
+                arg0->base.unk10.unk0 = val;
             }
         }
-        arg0->unk10.unk8 |= 0x80000;
+        arg0->base.unk10.unk8 |= 0x80000;
     }
     if (parent->unkC & 0x10) {
-        if (gUnk_02020EE0[gUnk_0203AD3C].base.unk60 == arg0->unk60) {
-            if (arg0->unk10.unk1F == 0) {
-                arg0->unk10.unk1F = sub_0803DF24(gUnk_08351648[50].unk8);
-                if (arg0->unk10.unk1F == 0xff) {
-                    arg0->unk10.unk1F = sub_0803DFAC(gUnk_08351648[50].unk8, 0);
+        if (gUnk_02020EE0[gUnk_0203AD3C].base.unk60 == arg0->base.unk60) {
+            if (arg0->base.unk10.unk1F == 0) {
+                arg0->base.unk10.unk1F = sub_0803DF24(gUnk_08351648[50].unk8);
+                if (arg0->base.unk10.unk1F == 0xff) {
+                    arg0->base.unk10.unk1F = sub_0803DFAC(gUnk_08351648[50].unk8, 0);
                 }
             }
         }
         else {
-            arg0->unk10.unk1F = 0;
+            arg0->base.unk10.unk1F = 0;
         }
     }
     else {
-        if (gUnk_02020EE0[gUnk_0203AD3C].base.unk60 == arg0->unk60) {
-            if (arg0->unk10.unk1F == 0) {
-                arg0->unk10.unk1F = sub_0803DF24(0x321);
-                if (arg0->unk10.unk1F == 0xff) {
-                    arg0->unk10.unk1F = sub_0803DFAC(0x321, 0);
+        if (gUnk_02020EE0[gUnk_0203AD3C].base.unk60 == arg0->base.unk60) {
+            if (arg0->base.unk10.unk1F == 0) {
+                arg0->base.unk10.unk1F = sub_0803DF24(0x321);
+                if (arg0->base.unk10.unk1F == 0xff) {
+                    arg0->base.unk10.unk1F = sub_0803DFAC(0x321, 0);
                 }
             }
         }
         else {
-            arg0->unk10.unk1F = parent->unkC & 0x10;
+            arg0->base.unk10.unk1F = parent->unkC & 0x10;
         }
     }
-    if (arg0->unk88 != 0 && arg0->flags & 2) {
-        arg0->flags |= 0x1000;
+    if (arg0->unk88 != 0 && arg0->base.flags & 2) {
+        arg0->base.flags |= 0x1000;
         return 1;
     }
     else {
@@ -457,10 +457,10 @@ void sub_080B2D68(struct Object2* arg0) {
 }
 
 u32 sub_080B2D74(struct Laser* arg0) {
-    arg0->unk10.unk1A = 7;
+    arg0->base.unk10.unk1A = 7;
     arg0->unk88 = 1;
-    arg0->flags &= ~0x200;
-    arg0->flags &= ~0x100;
-    arg0->xspeed = 0;
+    arg0->base.flags &= ~0x200;
+    arg0->base.flags &= ~0x100;
+    arg0->base.xspeed = 0;
     return 0;
 }
