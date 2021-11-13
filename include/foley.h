67279ecd35fa291ff35ae3cfd55082c05ca4efc9
Zhengwen Jiang <jian0463@umn.edu>
2021-11-13 14:35:40 -0600

    decompile foley

---
+++ include/foley.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_FOLEY_H
+#define GUARD_FOLEY_H
+
+#include "global.h"
+#include "data.h"
+
+void *CreateFoley(struct Object *, u8);
+void *CreateFoleyLeaves(struct Object *, u8);
+
+#endif
