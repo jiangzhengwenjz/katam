8f0899c2d43098da0365703a0d492f98e942d4f2
Solitai7e <p5ychomanti5@protonmail.com>
2022-05-28 13:49:52 +0000

    Decompile more of code_08002BA8.s

--- include/code_0800A868.h
+++ include/code_0800A868.h
@@ -4,8 +4,11 @@
 #include "global.h"
 #include "data.h"
 
-extern void *CreateObject(u8 playerId, struct Object *obj);
-extern void sub_0800A8BC(void);
+extern void sub_0800A6E8(void);
+extern void sub_0800A820(void);
+extern void sub_0800A8BC(struct Task *);
+extern void sub_0800A84C(void);
+extern void *CreateObject(u8 playerId, const struct Object *obj);
 extern void sub_0800A8C8(u8 arg0, u8 arg1, u8 arg2);
 extern void sub_0800A8F8(u8 arg0, u8 arg1, u8 arg2);
 
