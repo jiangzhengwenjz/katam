5ef94075068b7094cf6749bdd1b59da3e6c36eb7
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-06-07 19:42:48 -0400

    match master_sword.c

---
+++ include/master_sword.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_MASTERSWORD_H
+#define GUARD_MASTERSWORD_H
+
+#include "data.h"
+#include "global.h"
+#include "object.h"
+
+struct Object2* CreateMasterSwordNonCollectible(struct Object* arg0, u8 arg1);
+
+#endif
