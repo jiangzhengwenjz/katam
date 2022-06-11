aac012a6de396c62b930b62078fe278263fda735
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-06-11 17:35:01 -0400

    match fire.c
    did this fully solo!

---
+++ include/fire.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_FIRE_H
+#define GUARD_FIRE_H
+
+#include "data.h"
+#include "global.h"
+#include "object.h"
+
+void* CreateFire(struct Object* arg0, u8 arg1);
+
+#endif
---
+++ src/fire.c
@@ -0,0 +1,29 @@
+#include "fire.h"
+#include "kirby.h"
+#include "functions.h"
+
+static void FireCounterInit(struct Object2* arg0); // declaration
+static void FireCounterIncrement(struct Object2* arg0); // declaration
+
+void* CreateFire(struct Object* arg0, u8 arg1) {
+    struct Object2 *obj;
+    struct Task *task = TaskCreate(ObjectMain, sizeof(struct Object2), 0x1000, 0, ObjectDestroy);
+    obj = TaskGetStructPtr(task, obj);
+    InitObject(obj, arg0, arg1);
+    obj->base.flags |= 0xC000 << 1;
+    obj->base.unk5C |= 0x20;
+    sub_0803E2B0(&obj->base, -6, -12, 6, -3);
+    sub_0803E308(&obj->base, -7, -14, 7, -2);
+    ObjectInitSprite(obj);
+    gUnk_08351648[obj->type].unk10(obj);
+    return obj;
+}
+
+static void FireCounterInit(struct Object2* arg0) {
+    arg0->base.counter = 0;
+    arg0->unk78 = FireCounterIncrement;
+}
+
+static void FireCounterIncrement(struct Object2* arg0) {
+    arg0->base.counter++;
+}
