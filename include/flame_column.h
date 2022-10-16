ef314f38a3df1b8bac0bffa7940d1faa943e20f9
Zhengwen Jiang <jian0463@umn.edu>
2022-10-16 00:00:21 -0500

    decompile flame_column

---
+++ include/flame_column.h
@@ -0,0 +1,24 @@
+#ifndef GUARD_FLAME_COLUMN_H
+#define GUARD_FLAME_COLUMN_H
+
+#include "global.h"
+#include "data.h"
+
+struct FlameColumn
+{
+    struct Object2 obj2;
+    s16 unkB4;
+    u16 unkB6;
+    u16 unkB8;
+    s16 unkBA;
+    s16 unkBC;
+    u8 unkBE;
+    u8 unkBF;
+}; /* size = 0xC0 */
+
+void *CreateFlameColumn(struct Object *, u8);
+void sub_0811FDF0(struct FlameColumn *);
+
+extern const struct Unk_02021590 gUnk_08357CD8[];
+
+#endif
