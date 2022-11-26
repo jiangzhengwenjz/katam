e6535b5ab60e2c0c140bafb4b6966f782c17e477
Zhengwen Jiang <jian0463@umn.edu>
2022-11-26 02:07:59 -0600

    start decompiling master_hand

---
+++ include/master_hand.h
@@ -0,0 +1,15 @@
+#ifndef GUARD_MASTER_HAND_H
+#define GUARD_MASTER_HAND_H
+
+#include "global.h"
+#include "data.h"
+
+void *CreateMasterHand(struct Object *, u8);
+void *sub_080D4E78(struct Object *, u8);
+
+void sub_080D1A38(struct Object2 *);
+void sub_080D4F3C(struct Object2 *);
+
+extern const struct Unk_02021590 gUnk_08356108[];
+
+#endif
