10af3058d336566b08621ca7e97df1c887e563f1
Zhengwen Jiang <jian0463@umn.edu>
2023-01-21 19:49:53 -0600

    finish decompiling shard

---
+++ data/data_15.s
@@ -0,0 +1,22 @@
+	.include "asm/macros.inc"
+	.include "constants/constants.inc"
+
+	.section .rodata
+
+gUnk_082DE3E0:: @ 082DE3E0
+	.incbin "baserom.gba", 0x2DE3E0, 0x000002C
+
+gUnk_082DE40C:: @ 082DE40C
+	.incbin "baserom.gba", 0x2DE40C, 0x0000010
+
+gUnk_082DE41C:: @ 082DE41C
+	.incbin "baserom.gba", 0x2DE41C, 0x0000058
+
+gUnk_082DE474:: @ 082DE474
+	.incbin "baserom.gba", 0x2DE474, 0x000002C
+
+gUnk_082DE4A0:: @ 082DE4A0
+	.incbin "baserom.gba", 0x2DE4A0, 0x0000058
+
+gUnk_082DE4F8:: @ 082DE4F8
+	.incbin "baserom.gba", 0x2DE4F8, 0x00000B0
--- src/shard.c
+++ src/shard.c
@@ -5,10 +5,58 @@
 #include "random.h"
 #include "trig.h"
 
-struct Object4 *sub_0801C0A8(struct Shard *, u16);
-void sub_0801C3B0(struct Shard *);
-void sub_0801C3C4(struct Shard *);
-void sub_0801C4F0(struct Shard *);
+static struct Object4 *sub_0801C0A8(struct Shard *, u16);
+static void sub_0801C194(void);
+static void sub_0801C364(struct Shard *);
+static void sub_0801C388(struct Shard *);
+static void sub_0801C39C(struct Shard *);
+static void sub_0801C3B0(struct Shard *);
+static void sub_0801C3C4(struct Shard *);
+static void sub_0801C3E0(struct Shard *);
+static void sub_0801C3F4(struct Shard *);
+static void sub_0801C47C(struct Shard *);
+static void sub_0801C4A8(struct Shard *);
+static void sub_0801C4D4(struct Shard *);
+static void sub_0801C4F0(struct Shard *);
+static void sub_0801C50C(struct Shard *);
+static void sub_0801C52C(struct Shard *);
+static void sub_0801C558(struct Shard *);
+static void sub_0801C5CC(struct Shard *);
+static void sub_0801C618(struct Shard *);
+static void nullsub_27(struct Shard *);
+static void sub_0801C650(struct Shard *);
+static void sub_0801C66C(struct Shard *);
+static void sub_0801C6AC(struct Shard *);
+static void sub_0801C6C0(struct Shard *);
+static void sub_0801C6DC(struct Shard *);
+
+const struct Unk_02021590 gUnk_082DE5A8[] = {
+    { 0x2D4,   0, 0 },
+    { 0x2D4,   0, 0 },
+    { 0x2D4,   0, 0 },
+    { 0x2D4,   0, 0 },
+    { 0x2D4,   0, 0 },
+    { 0x2D4,   0, 0 },
+    { 0x2D4,   0, 0 },
+    { 0x2D4,   0, 0 },
+    { 0x2D3,   0, 0 },
+    { 0x2D3,   1, 0 },
+    { 0x2D3, 0xC, 0 },
+    { 0x2D3, 0xB, 0 },
+    { 0x2D3, 0xA, 0 },
+    { 0x2D3, 0xB, 0 },
+};
+
+static const u16 gUnk_082DE5E0[][3] = {
+    { 0x2D3, 2, 6 },
+    { 0x2D3, 3, 9 },
+    { 0x2D3, 4, 6 },
+    { 0x2D3, 5, 6 },
+    { 0x2D3, 6, 6 },
+    { 0x2D3, 7, 4 },
+    { 0x2D3, 8, 9 },
+    { 0x2D3, 9, 6 },
+};
 
 void *CreateShard(struct Object *template, u8 a2)
 {
@@ -94,7 +142,7 @@ bool32 sub_0801BA18(struct ObjectBase *objBase, bool32 a2)
     return TRUE;
 }
 
-bool32 sub_0801BB10(struct Shard *shard)
+static bool32 sub_0801BB10(struct Shard *shard)
 {
     u16 i;
     struct LevelInfo *li = gCurLevelInfo + shard->obj2.base.unk56;
@@ -111,7 +159,7 @@ bool32 sub_0801BB10(struct Shard *shard)
     return TRUE;
 }
 
-bool32 sub_0801BBA8(struct Shard *shard)
+static bool32 sub_0801BBA8(struct Shard *shard)
 {
     u16 i;
 
@@ -139,7 +187,7 @@ bool32 sub_0801BBA8(struct Shard *shard)
     }
 }
 
-void sub_0801BC28(struct Shard *shard)
+static void sub_0801BC28(struct Shard *shard)
 {
     struct Shard *shardAlias = shard;
     bool32 r2;
@@ -174,7 +222,7 @@ void sub_0801BC28(struct Shard *shard)
     shard->obj2.base.flags |= 4;
 }
 
-void sub_0801BD68(struct Shard *shard)
+static void sub_0801BD68(struct Shard *shard)
 {
     u8 array[4];
     struct Shard *shardAlias = shard;
@@ -204,7 +252,7 @@ void sub_0801BD68(struct Shard *shard)
     }
 }
 
-void sub_0801BE4C(struct Shard *shard) // see sub_0802AE9C
+static void sub_0801BE4C(struct Shard *shard) // see sub_0802AE9C
 {
     struct Shard *shardAlias = shard;
     u16 i;
@@ -254,4 +302,286 @@ void sub_0801BE4C(struct Shard *shard) // see sub_0802AE9C
         else
             shard->obj2.base.flags |= 4;
     }
+}
+
+static void sub_0801C004(struct Shard *shard)
+{
+    if (gCurLevelInfo[shard->obj2.base.unk56].currentRoom == gCurLevelInfo[gUnk_0203AD3C].currentRoom
+        && shard->obj2.base.sprite.palId)
+    {
+        struct Sprite sprite;
+
+        SpriteSomething(
+            &sprite,
+            0x6000000,
+            gUnk_082DE5A8[shard->obj2.unk83].unk0,
+            gUnk_082DE5A8[shard->obj2.unk83].unk2,
+            0xFF,
+            0,
+            0,
+            0,
+            0,
+            0x10,
+            shard->obj2.base.sprite.palId & 0xF,
+            0x80000
+        );
+    }
+}
+
+static struct Object4 *sub_0801C0A8(struct Shard *shard, u16 a2)
+{
+    struct Task *t = TaskCreate(sub_0801C194, sizeof(struct Object4), 0x3500, 0, sub_0803DCCC); // in IWRAM, weird
+    struct Object4 *obj4 = TaskGetStructPtr(t);
+
+    sub_0803E3B0(obj4);
+    obj4->unk0 = 3;
+    obj4->x = shard->obj2.base.x;
+    obj4->y = shard->obj2.base.y;
+    obj4->parent = shard;
+    obj4->roomId = shard->obj2.base.roomId;
+    obj4->x = shard->obj2.base.x;
+    obj4->y = shard->obj2.base.y;
+    if (Macro_0810B1F4(&shard->obj2.base))
+        obj4->flags |= 0x2000;
+    sub_080709F8(obj4, &obj4->sprite, VramMalloc(gUnk_082DE5E0[a2][2]), gUnk_082DE5E0[a2][0], gUnk_082DE5E0[a2][1], 0x19);
+    obj4->sprite.palId = shard->obj2.base.sprite.palId;
+    return obj4;
+}
+
+static void sub_0801C194(void)
+{
+    struct Object4 *tmp = TaskGetStructPtr(gCurTask), *obj4 = tmp;
+    struct Shard *shard;
+
+    if (obj4->flags & 0x1000)
+        TaskDestroy(gCurTask);
+    else
+    {
+        shard = obj4->parent;
+        if (shard)
+        {
+            if (shard->obj2.base.unk0 && shard->obj2.base.flags & 0x1000)
+            {
+                obj4->parent = NULL;
+                shard = NULL;
+            }
+            if (!shard)
+                goto label;
+            if (Macro_0810B1F4(&shard->obj2.base) && !(obj4->flags & 0x2000))
+            {
+                sub_0803DBC8(obj4);
+                return;
+            }
+        }
+        else
+        {
+        label:
+            KirbySomething(obj4);
+        }
+        Macro_0809E55C(obj4);
+        sub_0806FAC8(obj4);
+    }
+}
+
+void sub_0801C308(struct Shard *shard)
+{
+    if (shard->unkD4 < 8)
+        shard->obj2.unk78 = sub_0801C364;
+    else
+    {
+        if (shard->obj2.object->unk22 & 1)
+            shard->obj2.unk78 = sub_0801C3E0;
+        else if (sub_0801BBA8(shard))
+            shard->obj2.unk78 = sub_0801C39C;
+        else
+            shard->obj2.unk78 = sub_0801C388;
+        shard->obj2.unk7C = sub_0801C004;
+    }
+}
+
+static void sub_0801C364(struct Shard *shard)
+{
+    shard->unkD6 = 0;
+    shard->obj2.unk78 = sub_0801C3F4;
+    shard->obj2.base.flags |= 0x200;
+    shard->obj2.base.flags |= 0x400;
+}
+
+static void sub_0801C388(struct Shard *shard)
+{
+    shard->obj2.unk83 = 8;
+    shard->obj2.unk78 = sub_0801C5CC;
+}
+
+static void sub_0801C39C(struct Shard *shard)
+{
+    shard->obj2.unk83 = 0xA;
+    shard->obj2.unk78 = sub_0801C618;
+}
+
+static void sub_0801C3B0(struct Shard *shard)
+{
+    shard->obj2.unk83 = 9;
+    shard->obj2.unk78 = nullsub_27;
+}
+
+static void sub_0801C3C4(struct Shard *shard)
+{
+    shard->obj2.unk83 = 0xC;
+    shard->obj2.base.flags &= ~6;
+    shard->obj2.unk78 = sub_0801C650;
+}
+
+static void sub_0801C3E0(struct Shard *shard)
+{
+    shard->obj2.unk83 = 0xD;
+    shard->obj2.unk78 = sub_0801C66C;
+}
+
+static void sub_0801C3F4(struct Shard *shard)
+{
+    struct Shard *shardAlias = shard;
+    u8 i;
+
+    for (i = 0; i < gUnk_0203AD44; ++i)
+    {
+        struct Kirby *kirby = gKirbys + i;
+
+        if (shard->obj2.base.roomId == kirby->base.base.base.roomId
+            && kirby->hp <= 0)
+            return;
+    }
+    shard->obj2.base.flags &= ~0x200;
+    shard->obj2.base.flags &= ~0x400;
+    sub_080953D4(&shardAlias->obj2.base, 0, 0);
+    shard->obj2.unk78 = sub_0801C47C;
+}
+
+static void sub_0801C47C(struct Shard *shard)
+{
+    shard->unkDC = 0;
+    sub_0808AE30(&shard->obj2.base, 0, 0x292, 0);
+    shard->obj2.unk78 = sub_0801C4A8;
+}
+
+static void sub_0801C4A8(struct Shard *shard)
+{
+    struct Shard *shardAlias = shard;
+
+    if (shard->unkDC++ > 0x3C)
+        shard->obj2.unk78 = sub_0801C4D4;
+    shardAlias->obj2.base.flags |= 4;
+}
+
+static void sub_0801C4D4(struct Shard *shard)
+{
+    shard->unkD6 = 0;
+    shard->obj2.base.flags |= 4;
+    shard->obj2.unk78 = sub_0801BC28;
+}
+
+static void sub_0801C4F0(struct Shard *shard)
+{
+    shard->obj2.base.xspeed = 0;
+    shard->obj2.base.yspeed = 0;
+    shard->unkDC = 0;
+    shard->obj2.unk78 = sub_0801C50C;
+}
+
+static void sub_0801C50C(struct Shard *shard)
+{
+    if (shard->unkDC++ > 0xA)
+        shard->obj2.unk78 = sub_0801C52C;
+}
+
+static void sub_0801C52C(struct Shard *shard)
+{
+    if (sub_080395D4())
+    {
+        sub_08039600(shard->unkD4 + 1);
+        shard->obj2.unk78 = sub_0801C558;
+    }
+}
+
+static void sub_0801C558(struct Shard *shard)
+{
+    struct Shard *shardAlias = shard;
+    u16 r2 = shard->obj2.base.roomId;
+    u16 i;
+
+    // the loop does nothing
+    for (i = 0; i < 4; ++i)
+    {
+        struct Kirby *kirby = gKirbys + i;
+
+        if (r2 == kirby->base.base.base.roomId)
+        {
+            ++kirby; --kirby;
+        }
+    }
+    shardAlias->unkDC = (Rand16() & 1) + 2;
+    shardAlias->unkE0 = 0;
+    shard->obj2.unk78 = sub_0801BD68;
+    sub_0801BD68(shard);
+}
+
+static void sub_0801C5CC(struct Shard *shard)
+{
+    if (sub_0801BBA8(shard))
+        shard->obj2.unk78 = sub_0801C6AC;
+    else
+    {
+        u16 i;
+
+        for (i = 0; i < 8; ++i)
+            if (shard->obj4[i])
+                shard->obj4[i]->sprite.palId = shard->obj2.base.sprite.palId;
+        shard->obj2.base.flags |= 4;
+    }
+}
+
+static void sub_0801C618(struct Shard *shard)
+{
+    if (sub_0801BA18(&shard->obj2.base, (shard->obj2.object->unk22 >> 1) & 1))
+        shard->obj2.unk78 = sub_0801C6C0;
+    else
+        shard->obj2.base.flags |= 4;
+}
+
+static void nullsub_27(struct Shard *shard)
+{}
+
+static void sub_0801C650(struct Shard *shard)
+{
+    if (shard->obj2.base.flags & 2)
+        shard->obj2.unk78 = sub_0801C39C;
+}
+
+static void sub_0801C66C(struct Shard *shard)
+{
+    if ((*sub_08002888(1, 9, 0xFF) & 0xF0000) >> 0x10 == 0xC)
+    {
+        shard->obj2.unk83 = 0xA;
+        shard->obj2.base.flags &= ~0x400;
+        shard->obj2.unk78 = sub_0801C39C;
+    }
+}
+
+static void sub_0801C6AC(struct Shard *shard)
+{
+    shard->obj2.unk83 = 9;
+    shard->obj2.unk78 = sub_0801BE4C;
+}
+
+static void sub_0801C6C0(struct Shard *shard)
+{
+    shard->obj2.unk83 = 0xB;
+    shard->obj2.base.flags &= ~6;
+    shard->obj2.unk78 = sub_0801C6DC;
+}
+
+static void sub_0801C6DC(struct Shard *shard)
+{
+    if (shard->obj2.base.flags & 2)
+        shard->obj2.unk78 = sub_0801C6AC;
 }
