80768bf7242e0b2767d2a1257dad8f7758d99d24
Zhengwen Jiang <jian0463@umn.edu>
2023-02-11 20:52:16 -0600

    decompile boxin

---
+++ include/boxin.h
@@ -0,0 +1,13 @@
+#ifndef GUARD_BOXIN_H
+#define GUARD_BOXIN_H
+
+#include "global.h"
+#include "data.h"
+
+void *CreateBoxin(struct Object *, u8);
+
+void sub_080B5838(struct Object2 *);
+
+extern const struct Unk_02021590 gUnk_0835409C[];
+
+#endif
