8d390270988bf08c34226f9cecf9348f7c3b18b1
Zhengwen Jiang <jian0463@umn.edu>
2024-07-17 01:57:09 -0500

    start decompiling code_0814EBE4

---
+++ include/code_0814EBE4.h
@@ -0,0 +1,12 @@
+#ifndef GUARD_CODE_0814EBE4_H
+#define GUARD_CODE_0814EBE4_H
+
+#include "global.h"
+
+struct Unk_0814EBE4 {
+    s32 unk0;
+    void (*unk4)(struct Unk_0814EBE4 *);
+    s16 unk8;
+}; /* size = 0xC */
+
+#endif
