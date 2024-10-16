6502c754ebe9ef8d2ab2bcba320b0c8f6374c420
Zhengwen Jiang <jian0463@umn.edu>
2024-10-16 02:03:28 -0500

    more code_0806F780

--- src/code_0806F780.c
+++ src/code_0806F780.c
@@ -3,6 +3,12 @@
 #include "kirby.h"
 #include "main.h"
 #include "random.h"
+#include "functions.h"
+
+void sub_08070404(void);
+void sub_08070B50(struct Task *);
+
+extern void (*const gUnk_08350C38[])(struct ObjectBase *);
 
 bool16 sub_0806F780(struct ObjectBase *a1) {
     if (a1->flags & 0x1000) {
@@ -43,4 +49,89 @@ bool16 sub_0806F780(struct ObjectBase *a1) {
         return TRUE;
     }
     return FALSE;
+}
+
+bool32 sub_0806F8BC(struct ObjectBase *a1) {
+    SetPointerSomething(a1);
+    Macro_080FC150(a1, &a1->sprite);
+    if (!(a1->flags & 0x400) && gKirbys[gUnk_0203AD3C].base.base.base.roomId == a1->roomId) {
+        a1->sprite.x += gUnk_0203AD18[0];
+        a1->sprite.y += gUnk_0203AD18[1];
+        Macro_0803DBC8(a1, &a1->sprite);
+    }
+    return FALSE;
+}
+
+bool32 sub_0806FAC8(struct Object4 *a1) {
+    if (!(a1->flags & 0x400)) {
+        Macro_080FC150(a1, &a1->sprite);
+        if (!(a1->flags & 0x400) && gKirbys[gUnk_0203AD3C].base.base.base.roomId == a1->roomId) {
+            a1->sprite.x += gUnk_0203AD18[0];
+            a1->sprite.y += gUnk_0203AD18[1];
+            Macro_0803DBC8(a1, &a1->sprite);
+        }
+    }
+    return FALSE;
+}
+
+void sub_0806FC70(struct ObjectBase *a1) {
+    a1->unk62 = 0;
+    if (!(a1->flags & 0x100)) {
+        u8 r4, r7 = a1->unk57;
+
+        a1->unk57 = sub_080023E4(a1->unk56, a1->x >> 0xC, a1->y >> 0xC);
+        a1->unk58 = gUnk_082D88B8[a1->unk57];
+        if (gUnk_082D88B8[a1->unk57] & 0x200
+            || (gUnk_082D88B8[a1->unk57] & 0xC && !(gUnk_082D88B8[a1->unk57] & 0xF0000000))) {
+            if ((r4 = gUnk_082D88B8[r7] >> 0x1C)) {
+                if (gUnk_082D88B8[a1->unk57] & 0xC) {
+                    if (a1->yspeed > 0 && r4 < 7) return;
+                    if (a1->xspeed < 0) {
+                        if (r4 < 4 || r4 > 9)
+                            return;
+                    } else if (a1->xspeed > 0) {
+                        if (!(r4 < 4 || r4 > 9))
+                            return;
+                    }
+                }
+                a1->unk57 = r7;
+                if (a1->unk48 >> 0xC != a1->x >> 0xC) {
+                    if (a1->xspeed & 0x8000) {
+                        a1->x &= 0xFFFFF000;
+                        a1->unk48 = a1->x += 0x1000;
+                    } else {
+                        a1->x &= 0xFFFFF000;
+                        a1->unk48 = a1->x -= 1;
+                    }
+                }
+                if (a1->unk4C >> 0xC != a1->y >> 0xC) {
+                    if (a1->yspeed & 0x8000) {
+                        a1->y &= 0xFFFFF000;
+                        a1->unk4C = a1->y -= 1;
+                    } else {
+                        a1->y &= 0xFFFFF000;
+                        a1->unk4C = a1->y += 0x1000;
+                    }
+                }
+                gUnk_08350C38[r4](a1);
+            } else if (!(gUnk_082D88B8[a1->unk57] & 0x200)) {
+                return;
+            }
+            a1->unk62 = 0xFF;
+        } else if ((r4 = gUnk_082D88B8[a1->unk57] >> 0x1C)) {
+            gUnk_08350C38[r4](a1);
+        }
+    }
+}
+
+void sub_0806FDF4(void) {
+    struct Unk_0806FDF4 *ptr;
+
+    gUnk_03000518 = TaskCreate(sub_08070404, sizeof(struct Unk_0806FDF4), 0xFFEF, TASK_USE_IWRAM, sub_08070B50);
+    ptr = TaskGetStructPtr(gUnk_03000518);
+    CpuFill16(0, ptr, sizeof(struct Unk_0806FDF4));
+    ptr->unk0[0].roomId = 0xFFFE;
+    ptr->unk0[1].roomId = 0xFFFE;
+    ptr->unk0[2].roomId = 0xFFFE;
+    ptr->unk0[3].roomId = 0xFFFE;
 }
