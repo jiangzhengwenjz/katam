fe93ff910bd33c157f75029cf381951467a63747
IoIxD <30945097+IoIxD@users.noreply.github.com>
2025-02-07 14:18:02 -0700

    unk10_[...] -> unk[num]
    Co-authored-by: jiangzhengwenjz <jian0463@umn.edu>

--- include/treasures.h
+++ include/treasures.h
@@ -9,8 +9,8 @@
 struct Treasures {
     u8 chestFields[0x10];
     u32 shardField;
-    u32 unk10_2;
-    u32 unk10_3;
+    u32 unk14;
+    u32 unk18;
     u32 bigChestField;
     u16 unk20;
 }; /* size = 0x24 */
