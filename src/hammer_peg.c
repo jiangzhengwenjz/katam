75c339b1978e35dd0eb9671eb8eb445131ec2c01
ventiph <benawesome20@gmail.com>
2025-03-17 15:34:12 -0400

    Updates from review

--- src/hammer_peg.c
+++ src/hammer_peg.c
@@ -1,28 +1,32 @@
 #include "hammer_peg.h"
 
+static void sub_08025368(struct Object2 *);
+
+static void sub_0802532C(struct Object2 *);
+
 void sub_08025320(struct Object2 *r0) {
     r0->unk78 = sub_0802532C;
 }
 
-void sub_0802532C(struct Object2 *object) {
+static void sub_0802532C(struct Object2 *object) {
     u8 ret_val = sub_080023E4(
         object->base.unk56,
-        (object->base.x << 4) >> 0x10,
-        (object->base.y << 4) >> 0x10);
+        object->base.x >> 0xC,
+        object->base.y >> 0xC);
 
-    if ((ret_val) != 0xf6)
+    if (ret_val != 0xf6)
     {
-      object->unk85 = 0;
-      object->unk83 = 1;
+        object->unk85 = 0;
+        object->unk83 = 1;
 
-      object->unk78 = sub_08025368;
+        object->unk78 = sub_08025368;
     }
 }
 
-void sub_08025368(struct Object2 *object)
+static void sub_08025368(struct Object2 *object)
 {
-    if ((u8)object->unk85++ > 4)
+    if (object->unk85++ > 4)
     {
-      object->base.flags = (object->base.flags) | (0x80 << 5);
+        object->base.flags = (object->base.flags) | (0x1000);
     }
 }
