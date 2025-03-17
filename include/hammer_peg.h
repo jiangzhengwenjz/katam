5ad56bbd43119e1cb0c82987ef0850a22ecdc4c8
ventiph <benawesome20@gmail.com>
2025-03-17 13:45:43 -0400

    Started hammer_peg.c
    Missed some files
    
    Removed debug file
    
    Fixed formatting

---
+++ include/hammer_peg.h
@@ -0,0 +1,14 @@
+#ifndef GUARD_HAMMER_PEG_H
+#define GUARD_HAMMER_PEG_H
+
+#include "global.h"
+#include "data.h"
+#include "functions.h"
+
+void sub_08025320(struct Object2 *);
+
+void sub_0802532C(struct Object2 *);
+
+void sub_08025368(struct Object2 *);
+
+#endif
