54896ed174a696f19092d5ed19f8f6e17a9ea5da
ventiph <benawesome20@gmail.com>
2025-03-17 17:43:52 -0400

    Added CreateHammerPeg function

--- include/hammer_peg.h
+++ include/hammer_peg.h
@@ -4,6 +4,10 @@
 #include "global.h"
 #include "data.h"
 #include "functions.h"
+#include "object.h"
+#include "kirby.h"
+
+struct Object2 *CreateHammerPeg(struct Object *, u8);
 
 void sub_08025320(struct Object2 *);
 
--- src/hammer_peg.c
+++ src/hammer_peg.c
@@ -4,6 +4,37 @@ static void sub_08025368(struct Object2 *);
 
 static void sub_0802532C(struct Object2 *);
 
+struct Object2 *CreateHammerPeg(struct Object *object, u8 r1)
+{
+    struct Task *newTask = TaskCreate(ObjectMain, 0xb4, 0x1000, 0, ObjectDestroy);
+    struct Object2 *newObject2;
+
+    if((newTask->flags & 0x10) != 0)
+    {
+        newObject2 = (struct Object2 *)((newTask->structOffset << 2) + 0x2000000);
+    }
+    else
+    {
+        newObject2 = (struct Object2 *)(newTask->structOffset + 0x3000000);
+    }
+
+    InitObject(newObject2, object, r1);
+
+    newObject2->unk85 = 0;
+    newObject2->base.flags |= 0x18b00;
+    newObject2->base.unk68 &= -8;
+
+    sub_0803E308(&newObject2->base, -8, -8, 8, 8);
+
+    newObject2->unk83 = 0;
+
+    ObjectInitSprite(newObject2);
+
+    gUnk_08351648[newObject2->type].unk10(newObject2);
+
+    return newObject2;
+}
+
 void sub_08025320(struct Object2 *r0)
 {
     r0->unk78 = sub_0802532C;
