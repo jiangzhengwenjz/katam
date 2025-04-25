03f2bd7b396e42ad9ac672c9676521d506c3788d
Someone0123-pas <Jemand0123@protonmail.com>
2025-04-25 18:06:31 +0200

    Decompiled sub_08126A78 to sub_08126C48

--- include/pause_world_map.h
+++ include/pause_world_map.h
@@ -3,6 +3,7 @@
 
 #include "global.h"
 #include "data.h"
+#include "task.h"
 
 struct UnkKirbyMapSprite {
     /* 0x00 */ struct Sprite unk0;
@@ -21,18 +22,19 @@ struct PauseWorldMapStruct {
     /* 0x20C */ s8 unk20C;
     /* 0x20D */ u8 unk20D;
     /* 0x20E */ s16 unk20E; // Probably some kind of counter
-    /* 0x210 */ u8 unk210;  // 0x01 if pause_help is following and 0x04 if pause_area_map is following
+    /* 0x210 */ u8 unk210;  // 0x01 if pause_help::sub_08124430 is following and 0x04 if pause_area_map::sub_081278D4 should be following
                             // - which is weird, can pause_area_map even be accessed from pause_world_map?
                             // For some reason, this must be u8, while Unk_0203ACC0::unkD must be s8,
                             // but values are assigned from one to another
-    /* 0x211 */ u8 unk211;
+    /* 0x211 */ s8 unk211;
     /* 0x212 */ u16 filler1;
     /* 0x214 */ struct Task* unk214;
 }; /* size = 0x218 */
 
 // Some kind of kirby-player struct
 struct Unk_0203ACC0 {
-    /* 0x00 */ u8 filler0[0xd];
+    /* 0x00 */ struct Task* unk0;
+    /* 0x04 */ u8 filler0[0x9];
     /* 0x0D */ s8 unkD;
     /* 0x0E */ u16 unkE;
     /* 0x10 */ u8 filler1[0x4];
@@ -50,14 +52,25 @@ struct Unk_0203ACC0 {
     }                                                                   \
 }
 
+struct Unk_08363748 {
+    /* 0x00 */ u16 unk0; // animId1
+    /* 0x02 */ u8 unk2; // variant1
+    /* 0x03 */ u8 filler0[0x31];
+    /* 0x34 */ u16 unk34; // animId0
+    /* 0x36 */ u8 unk36; // variant0
+    /* 0x37 */ u8 filler1;
+}; /* size = 0x38 */
+
 extern struct Unk_0203ACC0 gUnk_0203ACC0[]; // Most likely with 4 entries per player
 extern const void* gUnk_081E08FC;
 extern const void* gUnk_0835A3CC;
-extern const u16 gUnk_08359C08[];
+extern const u16 gUnk_08359C08[]; // Holds roomID of visited doors from 0x1 to 0xf
+extern const u16 gUnk_08359C28[];
 
 // Perhaps holds size of the following incbins
 extern const u8 gUnk_08359DD8[];
-// Hold Tilemap Data
+// Something with Tilemap Data
+// - encode offsets and ranges for CpuFill16
 extern const u16 gUnk_08359DE8[];
 extern const u16 gUnk_08359DEC[];
 extern const u16 gUnk_08359DF4[];
@@ -74,6 +87,29 @@ extern const u16 gUnk_08359E50[];
 extern const u16 gUnk_08359E60[];
 extern const u16 gUnk_08359E6C[];
 
+extern const struct Unk_08363748 gUnk_08363748[]; // Probably holds one struct per language
+
 void sub_08125A4C(u32);
+
+// Called in sub_08125828 if corresponding door has not yet been visited
+void sub_081265C8(void); 
+void sub_08126618(void);
+void sub_08126668(void);
+void sub_081266B8(void);
+void sub_08126708(void);
+void sub_08126758(void);
+void sub_081267A8(void);
+void sub_081267F8(void);
+void sub_08126848(void);
+void sub_08126898(void);
+void sub_081268E8(void);
+void sub_08126938(void);
+void sub_08126988(void);
+void sub_081269D8(void);
+void sub_08126A28(void);
+
+// Called, when pause_area_map(!) is opened
+void sub_08126B58(struct Sprite*, struct Sprite*, u8); // Called for each kirby
+void sub_08126C48(void);
 
 #endif
