54d1ec8962ac6b72359b3166dcbe813ca05aaad0
Ultra <ultra.public@proton.me>
2025-05-31 01:46:47 +0000

    Droppy

---
+++ include/droppy.h
@@ -0,0 +1,16 @@
+#ifndef GUARD_DROPPY_H
+#define GUARD_DROPPY_H
+
+#include "data.h"
+#include "functions.h"
+void* CreateDroppy(struct Object *arg0, u8 arg1);
+void sub_0809FE9C(struct Object2 *arg0);
+
+#define Macro_0809FF6C(obj2, xOffset, yOffset) \
+    (((obj2)->base.x + (xOffset)) <= gCurLevelInfo[(obj2)->base.unk56].unk50  \
+        && ((obj2)->base.x + (xOffset)) >= gCurLevelInfo[(obj2)->base.unk56].unk48 \
+        && ((obj2)->base.y + (yOffset)) <= gCurLevelInfo[(obj2)->base.unk56].unk54 \
+        && ((obj2)->base.y + (yOffset)) >= gCurLevelInfo[(obj2)->base.unk56].unk4C) 
+
+
+#endif
