ff0f1ed7b2ba218c0816ee15b1ec9f3b4238edf2
Someone0123-pas <Jemand0123@protonmail.com>
2025-07-19 20:23:18 +0200

    Integrated PNG files for backgrounds 31 to 35 The credit tilemaps include a lot of padding, which for now has been left as is.

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -1,2 +1,5 @@
 graphics/corrupted_save/tileset.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -num_tiles 97 -Wnum_tiles
+
+graphics/rooms/backgrounds/dimension_mirror_3.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -num_tiles 900 -Wnum_tiles
