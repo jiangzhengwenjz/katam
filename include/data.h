ea4d9b5ffb89868dd3578ad08978679862093642
soiryl-pas <Jemand0123@protonmail.com>
2026-02-23 22:39:11 +0100

    Tweak constness of `union Unk_03002E60` and related

--- include/data.h
+++ include/data.h
@@ -715,12 +715,12 @@ extern struct Background *gUnk_03002400[];
 extern u16 gUnk_03002E20[];
 
 union __attribute__((transparent_union)) Unk_03002E60 {
-    const struct RoomTiledBG *x;
+    struct RoomTiledBG *x;
     const struct TiledBg_082D7850_Full *y;
     const struct TiledBg_082D7850 *z;
 };
 
-extern union Unk_03002E60 *gUnk_03002E60; // see gUnk_082D8D74
+extern const union Unk_03002E60 *gUnk_03002E60; // see gUnk_082D8D74
 
 extern struct Sprite *gUnk_03006030[];
 
