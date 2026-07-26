14736d8d32cefb90fc1ea52225f4015d1b13019f
testyourmine <25854988+testyourmine@users.noreply.github.com>
2026-07-25 21:51:16 -0500

    match sub_08118D80
    Co-Authored-By: Zhengwen Jiang <jian0463@umn.edu>

--- src/boss_challenge_door.c
+++ src/boss_challenge_door.c
@@ -64,21 +64,13 @@ static void sub_08118C18(struct BossChallengeDoor *door) {
     }
 }
 
-#ifndef NONMATCHING
-NAKED static void sub_08118D80(struct BossChallengeDoor *door) {
-    asm(".include \"asm/nonmatching/sub_08118D80.inc\"");
-}
-#else
 static void sub_08118D80(struct BossChallengeDoor *door) {
     struct BossChallengeDoor *d2;
     u16 bossIdx;
-    struct LevelInfo *levels;
     struct LevelInfo *level;
-    s32 x0, y0, w, h;
     u16 i;
     u16 j;
-    u16 k;
-    bool32 touched;
+    bool32 touched = FALSE;
     const u16 *panim;
     u8 sp28, sp29;
 
@@ -86,86 +78,67 @@ static void sub_08118D80(struct BossChallengeDoor *door) {
     door->obj2.base.flags |= 4;
     bossIdx = (*sub_08002888(1, 9, 0xFF) & 0xF0000) >> 16;
     if (gCurLevelInfo[door->obj2.base.unk56].currentRoom == gCurLevelInfo[gUnk_0203AD3C].currentRoom) {
-        if (door->unkB8->sprite.palId == 0) {
-            if (gKirbys[gUnk_0203AD3C].base.base.base.roomId == door->unkB8->roomId) {
-                u16 anim2 = gUnk_08357B3E[bossIdx][0];
-
-                door->unkB8->sprite.palId = sub_0803DF24(anim2);
-                if (door->unkB8->sprite.palId == 0xFF)
-                    door->unkB8->sprite.palId = sub_0803DFAC(anim2, (u8)gUnk_08357B3E[bossIdx][1]);
-            }
-            else {
-                door->unkB8->sprite.palId = 0;
-            }
+        if (d2->unkB8->sprite.palId == 0) {
+            Macro_081050E8(d2->unkB8, &d2->unkB8->sprite, gUnk_08357B3E[bossIdx][0], gUnk_08357B3E[bossIdx][1], 1);
         }
         if (d2->unkB8->sprite.palId != 0) {
             struct Sprite spr;
 
-            spr.tilesVram = 0x6000000;
-            spr.animId = gUnk_08357B3E[bossIdx][0];
-            spr.variant = gUnk_08357B3E[bossIdx][1];
-            spr.unk1B = 0xFF;
-            spr.x = 0;
-            spr.y = 0;
-            spr.unk14 = 0;
-            spr.unk16 = 0;
-            spr.unk1C = 0x10;
-            spr.palId = door->unkB8->sprite.palId & 0xF;
-            spr.unk8 = 0x80000;
-            sub_08155128(&spr);
-            sub_0803D280((spr.palId << 28) >> 24, 0x10);
+            SpriteSomething(&spr, 0x6000000, gUnk_08357B3E[bossIdx][0], gUnk_08357B3E[bossIdx][1], 0xFF, 0, 0, 0, 0, 0x10, d2->unkB8->sprite.palId & 0xF, 0x80000);
         }
     }
 
     level = &gCurLevelInfo[door->obj2.base.unk56];
-    x0 = door->obj2.base.x + (door->obj2.base.unk3C * 0x100);
-    y0 = door->obj2.base.y + (door->obj2.base.unk3D * 0x100);
-    w = (door->obj2.base.unk3E - door->obj2.base.unk3C) * 0x100;
-    h = (door->obj2.base.unk3F - door->obj2.base.unk3D) * 0x100;
-    levels = gCurLevelInfo;
-    touched = FALSE;
-    for (i = 0; i < gUnk_0203AD30; i++) {
-        struct Kirby *kirby = &gKirbys[i];
-
-        if (level->currentRoom != levels[i].currentRoom)
-            continue;
-        if (!(kirby->movementState & 0x40))
-            continue;
-        if (!(kirby->base.base.base.unkC & 0x1000))
-            continue;
-        if (x0 > kirby->base.base.base.x)
-            continue;
-        if (x0 + w < kirby->base.base.base.x)
-            continue;
-        if (y0 > kirby->base.base.base.y)
-            continue;
-        if (y0 + h >= kirby->base.base.base.y) {
-            touched = TRUE;
-            break;
+    {
+        struct S32Vec2 pos = {
+            .x = door->obj2.base.x + (door->obj2.base.unk3C * 0x100),
+            .y = door->obj2.base.y + (door->obj2.base.unk3D * 0x100),
+        };
+        struct S32Vec2 measure = {
+            .x = (door->obj2.base.unk3E - door->obj2.base.unk3C) * 0x100,
+            .y = (door->obj2.base.unk3F - door->obj2.base.unk3D) * 0x100,
+        };
+        touched = FALSE;
+        for (i = 0; i < gUnk_0203AD30; i++) {
+            struct Kirby *kirby = &gKirbys[i];
+    
+            if (level->currentRoom != gCurLevelInfo[i].currentRoom)
+                continue;
+            if (!(kirby->movementState & 0x40))
+                continue;
+            if (!(kirby->base.base.base.unkC & 0x1000))
+                continue;
+            if (pos.x > kirby->base.base.base.x)
+                continue;
+            if (pos.x + measure.x < kirby->base.base.base.x)
+                continue;
+            if (pos.y > kirby->base.base.base.y)
+                continue;
+            if (pos.y + measure.y >= kirby->base.base.base.y) {
+                touched = TRUE;
+                break;
+            }
         }
     }
     if (touched) {
+        u32 tmp;
         for (j = 0; j < gUnk_0203AD30; j++)
             ;
-        panim = &gUnk_08357B24[(*sub_08002888(1, 9, gCurLevelInfo[door->obj2.base.unk56].unk65E) & 0xF0000) >> 16];
-        for (k = 0; k < 4; k++) {
-            struct Kirby *kirby = &gKirbys[k];
-            u16 a;
-
+        tmp = (*sub_08002888(1, 9, gCurLevelInfo[door->obj2.base.unk56].unk65E) & 0xF0000) >> 16;
+        for (i = 0; i < 4; i++) {
+            struct Kirby *kirby = &gKirbys[i];
             if (kirby->hp <= 0)
                 continue;
             if (level->currentRoom != kirby->base.base.base.roomId)
                 continue;
-            a = *panim;
-            sub_08002D40(a, &sp28, &sp29);
-            gKirbys[k].base.base.base.unkC |= 0x800000;
-            sub_0805BDF4(kirby, a, sp28, sp29);
+            sub_08002D40(gUnk_08357B24[tmp], &sp28, &sp29);
+            gKirbys[i].base.base.base.unkC |= 0x800000;
+            sub_0805BDF4(kirby, gUnk_08357B24[tmp], sp28, sp29);
         }
         door->obj2.unk78 = sub_081191B4;
     }
     door->obj2.base.counter++;
 }
-#endif
 
 static void sub_08119094(struct BossChallengeDoor *door) {
     u32 *ptr;
